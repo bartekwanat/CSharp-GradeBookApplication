b03b6d8b5b7787f8da233cdc8a324a9093b30512
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-03-18 17:36:36 +0100

    Override CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -40,9 +40,14 @@ namespace GradeBook.GradeBooks
                 return 'D';
             }
             return 'F';
-            
+        }
 
-
-    }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            } else base.CalculateStatistics();
+        }
     }
 }
